247bb9878b4e6cf5d2ed82b0912c7fadde86475e
Ryan Pearson <ryanpearson015@gmail.com>
2021-09-22 13:52:04 -0500

    all routes completed, some comments added

---
+++ controllers/users.js
@@ -0,0 +1,116 @@
+const mysql = require('mysql');
+
+
+let pool = mysql.createPool({
+    connectionLimit: 100,
+    host: 'sql3.freesqldatabase.com',
+    user: 'sql3436006',
+    password: 'IvH8AQEy4l',
+    database: 'sql3436006',
+    debug: false
+  });
+
+function listUsers(req, res){
+    console.log('controller.users.list', req.params)
+    //select all users from Worker 
+    pool.query('SELECT * FROM Worker', function(err, rows){
+        if(err){
+            return res.json({
+                'error': true, 
+                'message': 'error occured:' + err
+        })
+    } else{
+        res.json(rows);
+    }
+  })
+}
+
+let showUser = function(req, res){
+    console.log('controller.users.show', req.params)
+    //code to return single user by id
+    //id is path param
+    let sql = "SELECT * FROM Worker WHERE WORKER_ID = ?";
+    //const replacements = []
+    sql = mysql.format(sql, [req.params.id]);
+    pool.query(sql, function(err, rows){
+        if(err){
+            return res.json({
+                'error': true, 
+                'message': 'error occured:' + err
+        })
+    } else{
+        res.json(rows);
+    }
+    })
+}
+
+
+//create user, assign it an id, add it to users array
+//content of user will be inside request body
+
+function createUser(req, res){
+    console.log('controller.users.create', req.body)
+    //SQL to create a new user using req.body
+    let sql = "INSERT INTO Worker (FIRST_NAME, LAST_NAME, SALARY, JOINING_DATE, DEPARTMENT)  VALUES (?, ?, ?, ?, ?)";
+    //const replacements = []
+    sql = mysql.format(sql, [req.body.FIRST_NAME, req.body.LAST_NAME, req.body.SALARY, req.body.JOINING_DATE, req.body.DEPARTMENT]);
+    //in the table i had uploaded and been using for practice, joining date had a weird format so that will start as 00:00:00, but everything else will work correctly
+    pool.query(sql, function(err, rows){
+        if(err){
+            return res.json({
+                'error': true, 
+                'message': 'error occured:' + err
+        })
+    } else{
+        res.json(rows);
+    }
+    })
+}
+
+let updateUser = function(req, res){
+    console.log('controller.users.update', req.body)
+    //code to update a user
+    //this will set a new salary for a specific worker
+    let sql = "UPDATE Worker SET SALARY = ? WHERE WORKER_ID = ?";
+    //const replacements = []
+    sql = mysql.format(sql, [req.body.SALARY, req.params.id]);
+    pool.query(sql, function(err, rows){
+        if(err){
+            return res.json({
+                'error': true, 
+                'message': 'error occured:' + err
+        })
+    } else{
+        res.json(rows);
+    }
+    })
+ 
+}
+
+let deleteUser = function(req, res){
+    console.log('controller.users.delete', req.body)
+     //code to delete a user from the database
+     let sql = "DELETE from Worker where WORKER_ID = ?";
+     //const replacements = []
+     sql = mysql.format(sql, [req.body.WORKER_ID]);
+     pool.query(sql, function(err, rows){
+         if(err){
+             return res.json({
+                 'error': true, 
+                 'message': 'error occured:' + err
+         })
+     } else{
+         res.json(rows);
+     }
+     })
+
+ 
+}
+
+module.exports = {
+    listUsers,
+    showUser,
+    createUser,
+    updateUser,
+    deleteUser
+}
--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 
+const mysql = require('mysql');
+
+app.use(express.json());
+
+//app.use(express.static('./public'));
+
+
+
 app.get('/', (req, res) => res.send('default route'))
+
+let usersRoutes = require('./routes/users');
+app.use(usersRoutes);
 
 app.listen(port, () => {
   console.log('app is listening on:', port)
 })
+
