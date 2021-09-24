19fc3187512023f1b27c380aa0d92b230c5dc3af
Ryan Pearson <ryanpearson015@gmail.com>
2021-09-23 19:09:29 -0500

    added init sql file and updated static html file

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 4000
 
 const mysql = require('mysql');
 
+app.use(express.static('./public'));
+
 app.use(express.json());
 
 //app.use(express.static('./public'));
---
+++ init.sql
@@ -0,0 +1,28 @@
+-- table initialization, taken from example I was using for more practice online
+-- came with additional tables to practice but this is the only one used for this project
+
+
+CREATE TABLE Worker (
+	WORKER_ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	FIRST_NAME CHAR(25),
+	LAST_NAME CHAR(25),
+	SALARY INT(15),
+	JOINING_DATE DATETIME,
+	DEPARTMENT CHAR(25)
+);
+
+INSERT INTO Worker 
+	(WORKER_ID, FIRST_NAME, LAST_NAME, SALARY, JOINING_DATE, DEPARTMENT) VALUES
+		(001, 'Monika', 'Arora', 100000, '14-02-20 09.00.00', 'HR'),
+		(002, 'Niharika', 'Verma', 80000, '14-06-11 09.00.00', 'Admin'),
+		(003, 'Vishal', 'Singhal', 300000, '14-02-20 09.00.00', 'HR'),
+		(004, 'Amitabh', 'Singh', 500000, '14-02-20 09.00.00', 'Admin'),
+		(005, 'Vivek', 'Bhati', 500000, '14-06-11 09.00.00', 'Admin'),
+		(006, 'Vipul', 'Diwan', 200000, '14-06-11 09.00.00', 'Account'),
+		(007, 'Satish', 'Kumar', 75000, '14-01-20 09.00.00', 'Account'),
+		(008, 'Geetika', 'Chauhan', 90000, '14-04-11 09.00.00', 'Admin');
+
+
+
+-- can update/delete/get any of these initial users, as well as adding more users
+
